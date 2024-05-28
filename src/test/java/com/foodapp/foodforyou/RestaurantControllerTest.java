3db74986046f783334cf940b382d1e76fef044cf
uriartej <uriarte.juan.j@gmail.com>
2024-05-28 12:57:34 -0700

    Created a file to test when getting restaurants by name.

--- src/test/java/com/foodapp/foodforyou/RestaurantControllerTest.java
+++ src/test/java/com/foodapp/foodforyou/RestaurantControllerTest.java
@@ -1,2 +1,40 @@
-package com.foodapp.foodforyou;public class RestaurantControllerTest {
+package com.foodapp.foodforyou;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RestaurantControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private RestaurantDatabase database;
+
+    @BeforeEach
+    public void setup() {
+        Restaurant restaurant = new Restaurant("Test Restaurant");
+        database.addRestaurant(restaurant);
+    }
+
+    @Test
+    public void getRestaurantByName_ShouldReturnOk() throws Exception {
+        mockMvc.perform(get("/restaurants/Test Restaurant"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getRestaurantByName_ShouldReturnNotFound() throws Exception {
+        mockMvc.perform(get("/restaurants/Nonexistent Restaurant"))
+                .andExpect(status().isNotFound());
+    }
 }
