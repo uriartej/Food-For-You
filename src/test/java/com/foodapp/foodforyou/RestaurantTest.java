59f9ef6f3603b385b38b2709b9b23922caa72661
uriartej <uriarte.juan.j@gmail.com>
2024-05-28 12:54:30 -0700

    Created file to test on add and remove menu item.

--- src/test/java/com/foodapp/foodforyou/RestaurantTest.java
+++ src/test/java/com/foodapp/foodforyou/RestaurantTest.java
@@ -1,2 +1,28 @@
-package com.foodapp.foodforyou;public class RestaurantTest {
+package com.foodapp.foodforyou;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class RestaurantTest {
+
+    @Test
+    public void testAddMenuItem() {
+        Restaurant restaurant = new Restaurant("Test Restaurant");
+        MenuItem item = new MenuItem("Test Item", 10.0, "Description", "Nutritional Info");
+        restaurant.addMenuItem(item);
+
+        assertEquals(1, restaurant.getMenuItems().size());
+        assertEquals("Test Item", restaurant.getMenuItems().get(0).getName());
+    }
+
+    @Test
+    public void testRemoveMenuItem() {
+        Restaurant restaurant = new Restaurant("Test Restaurant");
+        MenuItem item = new MenuItem("Test Item", 10.0, "Description", "Nutritional Info");
+        restaurant.addMenuItem(item);
+        restaurant.removeMenuItem(item);
+
+        assertEquals(0, restaurant.getMenuItems().size());
+    }
 }
