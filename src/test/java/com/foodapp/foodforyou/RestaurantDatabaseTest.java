6893c9b9a28ed4a394cb89140ff680d7123cb345
uriartej <uriarte.juan.j@gmail.com>
2024-05-30 16:49:14 -0700

    Deleted import static statements and input Assertions.*

--- src/test/java/com/foodapp/foodforyou/RestaurantDatabaseTest.java
+++ src/test/java/com/foodapp/foodforyou/RestaurantDatabaseTest.java
@@ -1,9 +1,7 @@
 package com.foodapp.foodforyou;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RestaurantDatabaseTest {
 
