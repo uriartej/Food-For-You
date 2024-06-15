93e2f8ecf319933d84c7d3bb0741f51327112e06
uriartej <uriarte.juan.j@gmail.com>
2024-06-15 08:20:30 -0700

    Updated file.

--- frontend/src/Restaurants.js
+++ frontend/src/Restaurants.js
@@ -1,34 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const Restaurants = () => {
-    const [restaurants, setRestaurants] = useState([]);
-
-    useEffect(() => {
-        axios.get('http://localhost:8080/restaurants')
-            .then(response => {
-                setRestaurants(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error fetching data.', error);
-            });
-            
-    }, []);
-
+function Restaurants({ restaurants }) {
     return (
         <div>
-            <h1>Restaurants</h1>
-            {restaurants.length > 0 ? (
-            <u1>
+            <h2>Restaurants</h2>
+            <ul>
                 {restaurants.map((restaurant, index) => (
-                    <li key = {index}>{restaurant.name}</li>
+                    <li key={index}>
+                        <h3>{restaurant.name}</h3>
+                        <ul>
+                            {restaurant.menuItems.map((item, idx) => (
+                                <li key={idx}>
+                                    <p>{item.name}</p>
+                                    <p>Price: {item.price}</p>
+                                    <p>Description: {item.description}</p>
+                                    <p>Nutritional Information: {item.nutritionalInformation}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
                 ))}
-            </u1>
-            ) : (
-                <p>Loading restaurants ...</p>
-            )}
+            </ul>
         </div>
     );
-};
+}
 
 export default Restaurants;
