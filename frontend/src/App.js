b73454ed13e6b657b43ad5ae4802763237dc6201
uriartej <uriarte.juan.j@gmail.com>
2024-06-19 19:00:50 -0700

    Added search bar to file.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
+// src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import RestaurantList from './RestaurantList';
+import RestaurantForm from './RestaurantForm';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Food Choices For You</h1>
-            </header>
-            <RestaurantList />
-        </div>
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <h1>Food Choices For You</h1>
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/add-restaurant">Add Restaurant</Link>
+                    </nav>
+                </header>
+                <Routes>
+                    <Route path="/" element={<RestaurantList />} />
+                    <Route path="/add-restaurant" element={<RestaurantForm />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
