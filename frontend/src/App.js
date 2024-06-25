ed00c8b73a94c928840cde0f0d7fd4dabc4ed81b
uriartej <uriarte.juan.j@gmail.com>
2024-06-25 12:07:49 -0700

    Updated file to work with filtering items.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import axios from 'axios';
 import RestaurantList from './RestaurantList';
 import RestaurantForm from './RestaurantForm';
+import SearchBar from './SearchBar';
+import FilterMenu from './FilterMenu';
+import './App.css';
 
 function App() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filterChoice, setFilterChoice] = useState('all');
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/restaurants')
+            .then(response => {
+                setRestaurants(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error fetching the restaurants!', error);
+            });
+    }, []);
+
     return (
         <Router>
             <div className="App">
@@ -17,7 +33,13 @@ function App() {
                     </nav>
                 </header>
                 <Routes>
-                    <Route path="/" element={<RestaurantList />} />
+                    <Route path="/" element={
+                        <>
+                            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                            <FilterMenu setFilterChoice={setFilterChoice} />
+                            <RestaurantList searchTerm={searchTerm} filterChoice={filterChoice} restaurants={restaurants} />
+                        </>
+                    } />
                     <Route path="/add-restaurant" element={<RestaurantForm />} />
                 </Routes>
             </div>
